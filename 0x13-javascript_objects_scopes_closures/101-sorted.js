91b076a40b10ae4d6f0199ab826e856acb8737d3
amde-et <99601438+amde-et@users.noreply.github.com>
2022-09-07 22:21:33 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,13 @@
-b
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const newDict = {};
+
+Object.keys(dict).map(function (key) {
+  if (!Array.isArray(newDict[dict[key]])) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
+});
+
+console.log(newDict);
