478bf0a381252af41616aa6f888e7cf7e7775012
amde-et <99601438+amde-et@users.noreply.github.com>
2022-09-23 19:33:18 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """
- script that fetches https://intranet.hbtn.io/status
+    Script that fetches https://intranet.htbtn.io/status
 """
 
 import requests
 
-if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    req = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(req.text)))
-    print('\t- content: {}'.format(req.text))
 
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
